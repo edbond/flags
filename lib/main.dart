d6672131bf1715a1811e543561a0dd8a643592d5
Eduard Bondarenko <1044+edbond@users.noreply.github.com>
2026-04-04 12:10:56 -0500

    chore: remove unused deps (cupertino_icons, sqflite, flutter_native_splash, equatable)

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'screens/quiz.dart';
 
 void main() {
-  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
   runApp(MyApp());
 }
 
